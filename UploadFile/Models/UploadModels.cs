28adfa19f564e7510a76bcc7fec98532eef364db
LopesJoaoG <73569153+LopesJoaoG@users.noreply.github.com>
2022-06-02 14:12:24 -0300

    Adicionar arquivos de projeto.

---
+++ UploadFile/Models/UploadModels.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadFile.Models
+{
+    public class ResponseModel
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public bool IsResponse { get; set; }
+    }
+
+    public class SingleFileModel : ResponseModel
+    {
+        [Required(ErrorMessage = "Please enter file name")]
+        public string FileName { get; set; }
+        [Required(ErrorMessage = "Please select file")]
+        public IFormFile File { get; set; }
+    }
+
+    public class MultipleFilesModel : ResponseModel
+    {
+        [Required(ErrorMessage = "Please select files")]
+        public List<IFormFile> Files { get; set; }
+    }
+
+
+}
